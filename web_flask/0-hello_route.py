4bd42c86083ea5ca7bbf6b9ddf41ee803506f8fe
daniloromero <1601@holbertonschool.com>
2021-01-19 13:27:09 -0500

    First commit

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""script that starts Flask web applicationlistening on 0.0.0.0 port 5000"""
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_HBNB():
+    """Hello HBNB!"""
+    return "Hello HBNB!"
